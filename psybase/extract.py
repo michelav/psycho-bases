1d1a9c2fee033927de0c9482a29e4ff0307fdd7a
Michel Vasconcelos <michel.vasconcelos@gmail.com>
2016-02-09 09:45:07 -0300

    Adding support do Lilacs base. Using sax parsing.

--- psybase/extract.py
+++ psybase/extract.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 import yaml
 import argparse
-from logging import config, getLogger
+from logging import config
 from extractors import DefaultExtractor
+from pathlib import Path
 
 def extractor_constructor(loader, node):
-    options = loader.construct_mapping(node)
-    name = options.pop('name')
-    url = options.pop('url')
-    ext = DefaultExtractor(name, url)
-    ext.output = options['format']
+    params = loader.construct_mapping(node)
+    name = params['name']
+    url = params['url']
+    options = params['options']
+    ext = DefaultExtractor(name, url, options)
+    ext.output = params['output']
     return ext
+
+def interval(interval_str):
+    limits = interval_str.split('..')
+    my_interval = [str(x) for x in range(int(limits[0]), int(limits[1]) + 1)]
+    print(my_interval)
+    return my_interval
 
 # Parsing arguments
 parser = argparse.ArgumentParser()
@@ -20,6 +28,8 @@ parser.add_argument("-b", "--base", type=str, action="append", dest="bases",
 parser.add_argument("-a", "--arquivo", default="bases.csv",
     help="Nome do arquivo de saída")
 parser.add_argument("-c", "--config", default="config.yaml", dest="config")
+parser.add_argument("-i", "--interval", dest="interval", type=interval,
+    help="Intervalo de tempo a que se refere a consulta no formato YYYY..YYYY")
 args=parser.parse_args()
 
 def prepare_bases(bases):
@@ -34,14 +44,21 @@ def extract(bases):
     print(queried_bases)
     for base in queried_bases:
         extractor = bases_map[base]
-        extractor.query(args.query)
+        extractor.query(args.query, args.interval)
+
+def setup_dirs(dirs):
+    for place in dirs:
+        cur_dir = Path(place)
+        if not cur_dir.exists():
+            cur_dir.mkdir()
 
 def main():
     yaml.add_constructor(u'!extractor', extractor_constructor)
     with open(args.config, 'r') as f:
-        config = yaml.load(f)
-    extract(config['app']['bases'])
-
+        yaml_file = yaml.load(f)
+    setup_dirs(yaml_file['app']['dirs'])
+    config.dictConfig(yaml_file['logging'])
+    extract(yaml_file['app']['bases'])
 
 if __name__ == '__main__':
     main()
--- psybase/extractors.py
+++ psybase/extractors.py
@@ -1,5 +1,110 @@
 import requests
 from string import Template
+from bs4 import BeautifulSoup
+from lxml import etree
+from xml.sax.handler import ContentHandler
+from lxml.sax import saxify
+import logging
+
+""" Module-wide logger """
+logger = logging.getLogger(__name__)
+
+""" Fast pass for isEnableFor(DEBUG). """
+is_debug_enabled = logger.isEnabledFor(logging.DEBUG)
+
+class DefaultContentHandler(ContentHandler):
+
+    @property
+    def found(self):
+        return self._found
+
+    @property
+    def articles(self):
+        return self._articles
+
+    @property
+    def found(self):
+        return self._found
+
+    def _save_doc(self):
+        try:
+            self._title = self._doc['titulo_pt']
+        except KeyError:
+            try:
+                self._title = self._doc['titulo_pt']
+            except KeyError:
+                self._title = self._doc['titulo_en']
+
+        # if is_debug_enabled:
+        self._count = self._count + 1
+        logger.debug('** Saving document %s **', self._count)
+        logger.debug('Titulo => {}'.format(self._title))
+        for info, value in self._doc.items():
+            print('{} => {}'.format(info, value))
+        self._doc['status'] = 'aberto'
+        self._articles[self._title] = self._doc
+        # if is_debug_enabled:
+        logger.debug('Articles saved: %s', len(list(self._articles.keys())))
+
+
+    def _doc_tag(self, attributes):
+        if self._doc:
+            self._save_doc()
+        self._title = None
+        self._doc = {}
+
+    def _result_tag(self, attributes):
+        self._found = int(attributes[(None, 'numFound')])
+
+    def _arr_tag(self, attributes):
+        try:
+            self._current = self._attrs[attributes[(None, 'name')]]
+        except KeyError:
+            self._current = None
+
+    def _str_tag(self, attributes):
+        pass
+
+    def __init__(self):
+        self._current = None
+        self._found = 0
+        self._articles = {}
+        self._title = None
+        self._doc = {}
+        self._tags = {'result': self._result_tag,
+                      'doc': self._doc_tag, 'arr': self._arr_tag, 'str': self._str_tag}
+
+        self._attrs = {'ab_en': 'abstract_en', 'ab_es': 'abstract_es',
+                        'ab_pt': 'abstract_pt', 'au': 'autores', 'cp': 'pais',
+                        'da': 'ano', 'la': 'lingua', 'ti_en': 'titulo_en',
+                        'ti_pt': 'titulo_pt', 'ti_es': 'titulo_es'}
+
+        # if is_debug_enabled:
+        self._count = 0
+
+    def startElementNS(self, name, qname, attributes):
+        uri, localname = name
+        try:
+            self._tags[localname](attributes)
+        except KeyError:
+            self._current = None
+            pass
+
+    def characters(self, data):
+        if self._current == 'autores':
+            try:
+                # if is_debug_enabled:
+                logger.debug('Storing %s => %s', self._current, data)
+                authors = self._doc['autores']
+                authors.append(data)
+            except KeyError:
+                self._doc[self._current] = [data]
+        elif self._current == 'ano':
+            self._doc[self._current] = data[0:5]
+        elif self._current:
+            # if is_debug_enabled:
+            logger.debug('Storing %s => %s', self._current, data)
+            self._doc[self._current] = data
 
 class DefaultExtractor(object):
 
@@ -20,14 +125,26 @@ class DefaultExtractor(object):
         self._output = value
 
 
-    def __init__(self, name, url):
+    def __init__(self, name, url, options):
         self._name = name
         self._url = url
+        self._options = options
         self._output = None
 
-    def query(self, qstr):
-        print(self._url)
+    def query(self, qstr, interval):
+        query_opts = {"output":self._output, "from":'0', "qstr":qstr}
+        if interval:
+            year_cluster = '"{0}"'.format('" OR "'.join(interval))
+        else: # Last 3 years
+            cur_year = date.today().year
+            year_cluster = '"{0}"'.format('" OR "'.join([str(x) for x in range(cur_year -3, cur_year)]))
+        query_opts['interval'] = year_cluster
+        query_opts.update(self._options)
         template = Template(self._url)
-        query = template.substitute(output=self._output, count='0', qstr=qstr, base=self._name)
-        print(query)
-
+        query = template.substitute(query_opts)
+        response = requests.get(query)
+        root = etree.XML(str.encode(response.text, 'UTF-8'))
+        handler = DefaultContentHandler()
+        saxify(root, handler)
+        if is_debug_enabled:
+            logger.debug('Query used: %s\nFound: %s', query, handler.found)
