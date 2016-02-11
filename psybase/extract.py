c3128b9ff13005a4ee33e4b962ee45302d2ab14d
Michel Vasconcelos <michel.vasconcelos@gmail.com>
2016-02-10 22:02:22 -0300

    Generating csv

--- psybase/extract.py
+++ psybase/extract.py
@@ -4,6 +4,7 @@ import argparse
 from logging import config
 from extractors import DefaultExtractor
 from pathlib import Path
+import csv
 
 def extractor_constructor(loader, node):
     params = loader.construct_mapping(node)
@@ -17,7 +18,6 @@ def extractor_constructor(loader, node):
 def interval(interval_str):
     limits = interval_str.split('..')
     my_interval = [str(x) for x in range(int(limits[0]), int(limits[1]) + 1)]
-    print(my_interval)
     return my_interval
 
 # Parsing arguments
@@ -25,7 +25,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("query", help="Consulta a ser usada na base de pesquisa")
 parser.add_argument("-b", "--base", type=str, action="append", dest="bases",
     help="Base a ser usada na pesquisa")
-parser.add_argument("-a", "--arquivo", default="bases.csv",
+parser.add_argument("-a", "--arquivo", default="bases.csv", dest='arquivo',
     help="Nome do arquivo de saída")
 parser.add_argument("-c", "--config", default="config.yaml", dest="config")
 parser.add_argument("-i", "--interval", dest="interval", type=interval,
@@ -42,9 +42,46 @@ def extract(bases):
     bases_map = prepare_bases(bases)
     queried_bases =  args.bases if args.bases else bases_map.keys()
     print(queried_bases)
+    articles = []
     for base in queried_bases:
         extractor = bases_map[base]
-        extractor.query(args.query, args.interval)
+        queried = extractor.query(args.query, args.interval)
+        print('Queried: {}'.format(len(queried)))
+        articles.append(queried)
+    print('Articles: {}'.format(len(articles)))
+    return articles
+
+def write_csv(docs):
+    with open(args.arquivo, 'w') as csvfile:
+        fieldnames = list(docs[0].keys())
+        writer = csv.DictWriter(csvfile, delimiter='|', fieldnames=fieldnames)
+        for doc in docs:
+            writer.writerow(doc)
+
+def process_articles(articles):
+    processed_articles = []
+    temp_base = {}
+
+    # Copying first dict
+    for k, doc in articles[0].items():
+        print(doc)
+        processed_articles.append(doc)
+
+    # Finding Duplicates
+    temp_base.update(articles[0])
+    for i in range(1, len(articles)):
+        for title, doc in articles[i]:
+            try:
+                temp_base[title]
+                doc['status'] = 'duplicado'
+            except KeyError:
+                pass
+            finally:
+                print("adicionando ao que vai ser salvo")
+                processed_articles.append(doc)
+        temp_base.update(articles[i])
+    print('Articles: {}'.format(len(processed_articles)))
+    write_csv(processed_articles)
 
 def setup_dirs(dirs):
     for place in dirs:
@@ -58,7 +95,8 @@ def main():
         yaml_file = yaml.load(f)
     setup_dirs(yaml_file['app']['dirs'])
     config.dictConfig(yaml_file['logging'])
-    extract(yaml_file['app']['bases'])
+    articles = extract(yaml_file['app']['bases'])
+    process_articles(articles)
 
 if __name__ == '__main__':
     main()
--- psybase/extractors.py
+++ psybase/extractors.py
@@ -1,9 +1,9 @@
 import requests
 from string import Template
-from bs4 import BeautifulSoup
 from lxml import etree
 from xml.sax.handler import ContentHandler
 from lxml.sax import saxify
+from collections import OrderedDict
 import logging
 
 """ Module-wide logger """
@@ -26,23 +26,50 @@ class DefaultContentHandler(ContentHandler):
     def found(self):
         return self._found
 
-    def _save_doc(self):
+    def _get_title(self):
         try:
-            self._title = self._doc['titulo_pt']
+            title = self._doc['titulo_pt']
         except KeyError:
             try:
-                self._title = self._doc['titulo_pt']
+                title = self._doc['titulo_es']
             except KeyError:
-                self._title = self._doc['titulo_en']
+                title = self._doc['titulo_en']
+        return title
 
-        # if is_debug_enabled:
+    def _get_abstract(self):
+        try:
+            abstract = self._doc['resumo_pt']
+
+        except KeyError:
+            try:
+                abstract = self._doc['resumo_es']
+            except KeyError:
+                abstract = self._doc['resumo_en']
+        return abstract
+
+
+    def _save_doc(self):
         self._count = self._count + 1
         logger.debug('** Saving document %s **', self._count)
-        logger.debug('Titulo => {}'.format(self._title))
-        for info, value in self._doc.items():
-            print('{} => {}'.format(info, value))
-        self._doc['status'] = 'aberto'
-        self._articles[self._title] = self._doc
+        title = self._get_title()
+        logger.debug('Titulo => {}'.format(title))
+        abstract = self._get_abstract()
+        saved_doc = OrderedDict()
+        # saved_doc['base'] = self._name
+        # saved_doc['descritor'] = self._descriptor
+        saved_doc['titulo'] = title
+        saved_doc['resumo'] = abstract
+        saved_doc['autores'] = self._doc['autores']
+        logger.debug('Autores => {}'.format(self._doc['autores']))
+        saved_doc['ano'] = self._doc['ano'][0:4]
+        logger.debug('Ano => {}'.format(self._doc['ano']))
+        saved_doc['lingua'] = self._doc['lingua']
+        saved_doc['pais'] = self._doc['pais']
+        saved_doc['status'] = 'aberto'
+        saved_doc['categoria'] = ''
+        saved_doc['exclusao'] = ''
+
+        self._articles[title] = saved_doc
         # if is_debug_enabled:
         logger.debug('Articles saved: %s', len(list(self._articles.keys())))
 
@@ -63,7 +90,11 @@ class DefaultContentHandler(ContentHandler):
             self._current = None
 
     def _str_tag(self, attributes):
-        pass
+        try:
+            self._current = self._attrs[attributes[(None, 'name')]]
+        except KeyError:
+            pass
+
 
     def __init__(self):
         self._current = None
@@ -74,8 +105,8 @@ class DefaultContentHandler(ContentHandler):
         self._tags = {'result': self._result_tag,
                       'doc': self._doc_tag, 'arr': self._arr_tag, 'str': self._str_tag}
 
-        self._attrs = {'ab_en': 'abstract_en', 'ab_es': 'abstract_es',
-                        'ab_pt': 'abstract_pt', 'au': 'autores', 'cp': 'pais',
+        self._attrs = {'ab_en': 'resumo_en', 'ab_es': 'resumo_es',
+                        'ab_pt': 'resumo_pt', 'au': 'autores', 'cp': 'pais',
                         'da': 'ano', 'la': 'lingua', 'ti_en': 'titulo_en',
                         'ti_pt': 'titulo_pt', 'ti_es': 'titulo_es'}
 
@@ -94,16 +125,14 @@ class DefaultContentHandler(ContentHandler):
         if self._current == 'autores':
             try:
                 # if is_debug_enabled:
-                logger.debug('Storing %s => %s', self._current, data)
+                # logger.debug('Storing %s => %s', self._current, data)
                 authors = self._doc['autores']
                 authors.append(data)
             except KeyError:
                 self._doc[self._current] = [data]
-        elif self._current == 'ano':
-            self._doc[self._current] = data[0:5]
         elif self._current:
             # if is_debug_enabled:
-            logger.debug('Storing %s => %s', self._current, data)
+            # logger.debug('Storing %s => %s', self._current, data)
             self._doc[self._current] = data
 
 class DefaultExtractor(object):
@@ -126,12 +155,16 @@ class DefaultExtractor(object):
 
 
     def __init__(self, name, url, options):
+        print(options)
         self._name = name
         self._url = url
+        self._descriptor = None
+        # self._base = options['db']
         self._options = options
         self._output = None
 
     def query(self, qstr, interval):
+        self._descriptor = qstr
         query_opts = {"output":self._output, "from":'0', "qstr":qstr}
         if interval:
             year_cluster = '"{0}"'.format('" OR "'.join(interval))
@@ -142,9 +175,12 @@ class DefaultExtractor(object):
         query_opts.update(self._options)
         template = Template(self._url)
         query = template.substitute(query_opts)
+        logger.debug('Querying base: %s\n', self._options['db'])
         response = requests.get(query)
         root = etree.XML(str.encode(response.text, 'UTF-8'))
         handler = DefaultContentHandler()
         saxify(root, handler)
-        if is_debug_enabled:
-            logger.debug('Query used: %s\nFound: %s', query, handler.found)
+        # if is_debug_enabled:
+        logger.debug('Query used: %s\nFound: %s', query, handler.found)
+        logger.debug('Len: %s', len(handler.articles))
+        return handler.articles
